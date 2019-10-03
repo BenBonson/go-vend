acf7ece44f808884f57bd49468ac019674cf177d
Mehrab Rahman <mehrab.rahman@revature.com>
2019-10-03 11:43:35 -0500

    adding structs and interfaces

--- main.go
+++ main.go
@@ -3,5 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello go")
+	var cewmachine = machine{
+		model: "18239342",
+		address: address{
+			street: "101 Mitchell St",
+			city:   "Arlington",
+			state:  "TX",
+			zip:    "76010",
+		},
+		capacity: 100,
+	}
+
+	//fmt.Println(cewmachine)
+	//fmt.Println("Capacity:", cewmachine.capacity)
+
+	var p printer = cewmachine
+	var a printer = cewmachine.address
+	fmt.Println(p)
+	fmt.Println(a)
 }
